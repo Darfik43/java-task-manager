41fae67a76c13ebb40dfa2b2517069f823818e80
Darfik43 <darfik43@gmail.com>
2024-08-14 21:45:29 +0300

    feat(repo): create repo for User model

---
+++ src/main/java/com/darfik/taskmanager/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.darfik.taskmanager.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
