e7050f337d22e12e69b412c968dc524de6226c14
Darfik43 <darfik43@gmail.com>
2024-08-16 22:22:45 +0300

    feat(auth): create 2 method to generate Token

--- src/main/java/com/darfik/taskmanager/config/JwtService.java
+++ src/main/java/com/darfik/taskmanager/config/JwtService.java
@@ -2,11 +2,16 @@ package com.darfik.taskmanager.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -15,6 +20,23 @@ public class JwtService {
     private static final String SECRET_KEY = "BPGysAUJi7zLQwMkRtphnbLJ9LLtlXPv";
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails user
+    ) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSigninKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails user) {
+        return generateToken(new HashMap<>(), user);
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
