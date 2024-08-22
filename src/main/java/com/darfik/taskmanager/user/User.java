618c51b9e26e2919e727ece749e933f37cfa041f
Darfik43 <darfik43@gmail.com>
2024-08-22 23:34:03 +0300

    remove(model): remove userdetails logic out of user entity

--- src/main/java/com/darfik/taskmanager/user/User.java
+++ src/main/java/com/darfik/taskmanager/user/User.java
@@ -1,61 +1,27 @@
 package com.darfik.taskmanager.user;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
-@NoArgsConstructor
 @Entity
-@Table(name = "_user")
-public class User implements UserDetails {
+@Table(name = "users")
+public class User {
 
     @Id
     @GeneratedValue
-    private Integer id;
-    private String firstname;
-    private String lastname;
+    private Long id;
     @Column(unique = true)
     private String email;
     private String password;
+
+    @ElementCollection
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Set<Role> roles;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
