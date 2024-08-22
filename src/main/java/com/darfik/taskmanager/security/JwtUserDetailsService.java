93c1c72e6f6fc8e13b7ecb05280a2c12164ae715
Darfik43 <darfik43@gmail.com>
2024-08-22 23:50:15 +0300

    feat(security): create JwtUserDetailsService for authentication

---
+++ src/main/java/com/darfik/taskmanager/security/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.darfik.taskmanager.security;
+
+import com.darfik.taskmanager.security.JwtEntityFactory;
+import com.darfik.taskmanager.service.UserService;
+import com.darfik.taskmanager.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUsername(username);
+        return JwtEntityFactory.create(user);
+    }
+}
