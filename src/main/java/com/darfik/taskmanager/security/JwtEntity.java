659ad96b9c866d6de7400694e4ff84cbd0684955
Darfik43 <darfik43@gmail.com>
2024-08-22 23:39:36 +0300

    remove(security): remove name out of jwtentity

--- src/main/java/com/darfik/taskmanager/security/JwtEntity.java
+++ src/main/java/com/darfik/taskmanager/security/JwtEntity.java
@@ -15,7 +15,6 @@ public class JwtEntity implements UserDetails {
 
     private final Long id;
     private final String username;
-    private final String name;
     private final String password;
     private final Collection<? extends GrantedAuthority> authorities;
 
