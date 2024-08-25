be63d2e25966f7ee5f8e9c604b92a658602bd82c
Darfik43 <darfik43@gmail.com>
2024-08-25 20:21:00 +0300

    feat(security): start tokenProvider implementation

--- src/main/java/com/darfik/taskmanager/security/JwtTokenProvider.java
+++ src/main/java/com/darfik/taskmanager/security/JwtTokenProvider.java
@@ -1,10 +1,55 @@
 package com.darfik.taskmanager.security;
 
+import com.darfik.taskmanager.service.UserService;
+import com.darfik.taskmanager.service.props.JwtProperties;
+import com.darfik.taskmanager.user.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
+    private final JwtProperties jwtProperties;
 
+    private final UserDetailsService userDetailsService;
+    private final UserService userService;
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
+    }
+
+    public String createAccessToken(Long userId, String username, Set<Role> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("id", userId);
+        claims.put("roles", resolveRoles(roles));
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + jwtProperties.getAccess());
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(key)
+                .compact();
+    }
+
+    private List<String> resolveRoles(Set<Role> roles) {
+        return roles.stream()
+                .map(Enum::name)
+                .collect(Collectors.toList());
+    }
 
 }
