b3099e46ad09b46b1d46b1380ab8e380f2bcae46
Darfik43 <darfik43@gmail.com>
2024-08-26 20:16:01 +0300

    feat(security): update filter to validate authentication

--- src/main/java/com/darfik/taskmanager/security/JwtTokenFilter.java
+++ src/main/java/com/darfik/taskmanager/security/JwtTokenFilter.java
@@ -1,11 +1,38 @@
 package com.darfik.taskmanager.security;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.apache.kafka.common.errors.ResourceNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
 
-@Component
-@RequiredArgsConstructor
-public class JwtTokenFilter extends OncePerRequestFilter {
+import java.io.IOException;
 
+@AllArgsConstructor
+public class JwtTokenFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String bearerToken = ((HttpServletRequest) servletRequest).getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            bearerToken = bearerToken.substring(7);
+        }
+        if (bearerToken != null && jwtTokenProvider.validateToken(bearerToken)) {
+            try {
+                Authentication authentication = jwtTokenProvider.getAuthentication(bearerToken);
+                if (authentication != null) {
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (ResourceNotFoundException ignored) {
+
+            }
+        }
+    }
 }
