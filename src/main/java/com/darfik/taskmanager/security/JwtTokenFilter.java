9461c46b2ac0d18aa4afedd9f6ab33cff95e2de5
Darfik43 <darfik43@gmail.com>
2024-08-26 20:07:43 +0300

    refactor(security): rename JwtAuthenticationFilter.java -> JwtTokenFilter

---
+++ src/main/java/com/darfik/taskmanager/security/JwtTokenFilter.java
@@ -0,0 +1,11 @@
+package com.darfik.taskmanager.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+}
