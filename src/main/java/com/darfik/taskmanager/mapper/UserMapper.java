24ae9bc0c1376a400321cb90257a3f4941a930d3
Darfik43 <darfik43@gmail.com>
2024-08-27 19:43:36 +0300

    feat(mapper): update mapper via mapstruct

--- src/main/java/com/darfik/taskmanager/mapper/UserMapper.java
+++ src/main/java/com/darfik/taskmanager/mapper/UserMapper.java
@@ -1,31 +1,13 @@
 package com.darfik.taskmanager.mapper;
 
-import com.darfik.taskmanager.user.Role;
+import com.darfik.taskmanager.dto.UserDto;
 import com.darfik.taskmanager.user.User;
-import lombok.SneakyThrows;
+import org.mapstruct.Mapper;
 
-import java.sql.ResultSet;
-import java.util.HashSet;
-import java.util.Set;
+@Mapper(componentModel = "spring")
+public interface UserMapper {
 
-public class UserMapper {
-
-    @SneakyThrows
-    public static User mapRow(ResultSet resultSet) {
-        Set<Role> roles = new HashSet<>();
-        while (resultSet.next()) {
-            roles.add(Role.valueOf(resultSet.getString("user_role_role")));
-        }
-        resultSet.beforeFirst();
-        if (resultSet.next()) {
-            User user = new User();
-            user.setId(resultSet.getLong("user_id"));
-            user.setEmail(resultSet.getString("user_username"));
-            user.setPassword(resultSet.getString("user_password"));
-            user.setRoles(roles);
-            return user;
-        }
-        return null;
-    }
+    UserDto toDto(User user);
+    User toEntity(UserDto userDto);
 
 }
