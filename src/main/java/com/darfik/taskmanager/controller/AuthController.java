9820cabe1aa5fea9284e0bb056ecb648cd667da9
Darfik43 <darfik43@gmail.com>
2024-08-27 19:45:05 +0300

    feat(controller): create controller with login and register methods

---
+++ src/main/java/com/darfik/taskmanager/controller/AuthController.java
@@ -0,0 +1,40 @@
+package com.darfik.taskmanager.controller;
+
+import com.darfik.taskmanager.dto.UserDto;
+import com.darfik.taskmanager.dto.auth.JwtRequest;
+import com.darfik.taskmanager.dto.auth.JwtResponse;
+import com.darfik.taskmanager.mapper.UserMapper;
+import com.darfik.taskmanager.service.AuthService;
+import com.darfik.taskmanager.service.UserService;
+import com.darfik.taskmanager.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    private final UserService userService;
+
+    private final UserMapper userMapper;
+
+    @PostMapping("/login")
+    public JwtResponse login(@RequestBody JwtRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    @PostMapping("/register")
+    public UserDto register(@RequestBody UserDto userDto) {
+        User user = userMapper.toEntity(userDto);
+        User createdUser = userService.create(user);
+        return userMapper.toDto(createdUser);
+    }
+
+
+
+}
