71fd0ae94bc017950b70fbfd66ae6f452f1a4edb
Darfik43 <darfik43@gmail.com>
2024-08-27 19:41:54 +0300

    feat(mapper): update mapper via MapStruct

---
+++ src/main/java/com/darfik/taskmanager/dto/UserDto.java
@@ -0,0 +1,11 @@
+package com.darfik.taskmanager.dto;
+
+public class UserDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String passwordConfirmation;
+
+}
