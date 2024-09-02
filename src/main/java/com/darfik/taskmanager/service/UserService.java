6a2a01b64f5237a9138c50c1237212f2e4b1b603
Darfik43 <darfik43@gmail.com>
2024-09-02 18:44:53 +0300

    feat(service): service returns dto instead of model

--- src/main/java/com/darfik/taskmanager/service/UserService.java
+++ src/main/java/com/darfik/taskmanager/service/UserService.java
@@ -1,11 +1,12 @@
 package com.darfik.taskmanager.service;
 
+import com.darfik.taskmanager.dto.UserDto;
 import com.darfik.taskmanager.user.User;
 
 public interface UserService {
 
     User getById(Long id);
     User getByUsername(String username);
-    User create(User user);
+    UserDto create(UserDto userDto);
 
 }
