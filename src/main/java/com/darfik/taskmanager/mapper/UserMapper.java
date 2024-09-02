24763cc98a4e4986e4fccb2d6d97bfedb9df5ec2
Darfik43 <darfik43@gmail.com>
2024-09-02 18:45:26 +0300

    feat(service): add ignore password property

--- src/main/java/com/darfik/taskmanager/mapper/UserMapper.java
+++ src/main/java/com/darfik/taskmanager/mapper/UserMapper.java
@@ -3,11 +3,14 @@ package com.darfik.taskmanager.mapper;
 import com.darfik.taskmanager.dto.UserDto;
 import com.darfik.taskmanager.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "password", ignore = true)
     UserDto toDto(User user);
+
     User toEntity(UserDto userDto);
 
 }
