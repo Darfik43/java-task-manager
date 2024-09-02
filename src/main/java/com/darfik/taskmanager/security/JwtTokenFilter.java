f0b19ec376f8b80f249c6b5af14215dcf12ff5aa
Darfik43 <darfik43@gmail.com>
2024-09-02 18:53:17 +0300

    refactor(filter): add doFilter in the ending of the filter

--- src/main/java/com/darfik/taskmanager/security/JwtTokenFilter.java
+++ src/main/java/com/darfik/taskmanager/security/JwtTokenFilter.java
@@ -2,25 +2,24 @@ package com.darfik.taskmanager.security;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @AllArgsConstructor
-public class JwtTokenFilter extends GenericFilterBean {
+public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String bearerToken = ((HttpServletRequest) servletRequest).getHeader("Authorization");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String bearerToken = request.getHeader("Authorization");
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             bearerToken = bearerToken.substring(7);
         }
@@ -34,5 +33,6 @@ public class JwtTokenFilter extends GenericFilterBean {
 
             }
         }
+        filterChain.doFilter(request, response);
     }
 }
