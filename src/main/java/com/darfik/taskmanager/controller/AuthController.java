d2d108809c646d08c28af9fa998a35399cd8385b
Darfik43 <darfik43@gmail.com>
2024-09-02 18:47:12 +0300

    refactor(controller): refactor authController relating it returns dto instead of model

--- src/main/java/com/darfik/taskmanager/controller/AuthController.java
+++ src/main/java/com/darfik/taskmanager/controller/AuthController.java
@@ -3,10 +3,8 @@ package com.darfik.taskmanager.controller;
 import com.darfik.taskmanager.dto.UserDto;
 import com.darfik.taskmanager.dto.auth.JwtRequest;
 import com.darfik.taskmanager.dto.auth.JwtResponse;
-import com.darfik.taskmanager.mapper.UserMapper;
 import com.darfik.taskmanager.service.AuthService;
 import com.darfik.taskmanager.service.UserService;
-import com.darfik.taskmanager.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,8 +19,6 @@ public class AuthController {
     private final AuthService authService;
     private final UserService userService;
 
-    private final UserMapper userMapper;
-
     @PostMapping("/login")
     public JwtResponse login(@RequestBody JwtRequest loginRequest) {
         return authService.login(loginRequest);
@@ -30,9 +26,7 @@ public class AuthController {
 
     @PostMapping("/register")
     public UserDto register(@RequestBody UserDto userDto) {
-        User user = userMapper.toEntity(userDto);
-        User createdUser = userService.create(user);
-        return userMapper.toDto(createdUser);
+        return userService.create(userDto);
     }
 
     @PostMapping("/refresh")
