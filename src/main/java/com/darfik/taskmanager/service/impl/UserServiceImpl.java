4d342a6afed7de8c471649d66f6ec0c9a20db485
Darfik43 <darfik43@gmail.com>
2024-09-02 18:45:05 +0300

    feat(service): impl create method

--- src/main/java/com/darfik/taskmanager/service/impl/UserServiceImpl.java
+++ src/main/java/com/darfik/taskmanager/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package com.darfik.taskmanager.service.impl;
 
+import com.darfik.taskmanager.dto.UserDto;
+import com.darfik.taskmanager.mapper.UserMapper;
 import com.darfik.taskmanager.service.UserService;
+import com.darfik.taskmanager.user.Role;
 import com.darfik.taskmanager.user.User;
 import com.darfik.taskmanager.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +12,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Set;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
     @Override
     @Transactional(readOnly = true)
@@ -34,7 +40,21 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public User create(User user) {
-        return null;
+    public UserDto create(UserDto userDto) {
+        if (userRepository.existsByEmail(userDto.getEmail())) {
+            throw new IllegalStateException("This email is already taken");
+        }
+        if (!userDto.getPassword().equals(userDto.getPasswordConfirmation())) {
+            throw new IllegalStateException("Password and password confirmation do not match");
+        }
+
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        User newUser = userMapper.toEntity(userDto);
+        Set<Role> roles = Set.of(Role.USER);
+        newUser.setRoles(roles);
+
+        userRepository.save(newUser);
+
+        return userMapper.toDto(newUser);
     }
 }
