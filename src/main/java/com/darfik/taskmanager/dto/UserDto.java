ecc6e92ce8d3b3e97dc5cb2cb7de52165d8dda68
Darfik43 <darfik43@gmail.com>
2024-09-02 18:45:44 +0300

    feat(dto): add getters and setters annotation for dto

--- src/main/java/com/darfik/taskmanager/dto/UserDto.java
+++ src/main/java/com/darfik/taskmanager/dto/UserDto.java
@@ -1,9 +1,13 @@
 package com.darfik.taskmanager.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDto {
 
     private Long id;
-    private String name;
     private String email;
     private String password;
     private String passwordConfirmation;
