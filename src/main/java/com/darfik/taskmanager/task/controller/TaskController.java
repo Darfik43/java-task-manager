5b48b54620014779364922806c14c953378cbaee
Darfik43 <darfik43@gmail.com>
2024-09-15 22:03:06 +0300

    feat(controller): create controller for task

---
+++ src/main/java/com/darfik/taskmanager/task/controller/TaskController.java
@@ -0,0 +1,12 @@
+package com.darfik.taskmanager.task.controller;
+
+import com.darfik.taskmanager.task.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final TaskService taskService;
+}
