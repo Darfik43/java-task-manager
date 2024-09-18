7ff0452a8b9f6d98a4aa1d9d2b03167652844947
Darfik43 <darfik43@gmail.com>
2024-09-18 21:06:15 +0300

    feat(service): create findAllTasks method

--- src/main/java/com/darfik/taskmanager/task/service/DefaultTaskService.java
+++ src/main/java/com/darfik/taskmanager/task/service/DefaultTaskService.java
@@ -1,8 +1,12 @@
 package com.darfik.taskmanager.task.service;
 
+import com.darfik.taskmanager.task.entity.Task;
 import com.darfik.taskmanager.task.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +14,8 @@ public class DefaultTaskService implements TaskService {
 
     private final TaskRepository taskRepository;
 
+    @Override
+    public List<Task> findAllTasks() {
+        return Collections.unmodifiableList(taskRepository.findAll());
+    }
 }
