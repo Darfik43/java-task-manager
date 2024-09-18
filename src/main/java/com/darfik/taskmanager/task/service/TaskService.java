ec33c89833c666313753f89a3221e9adbc12dd5c
Darfik43 <darfik43@gmail.com>
2024-09-18 21:06:18 +0300

    feat(service): create findAllTasks method

--- src/main/java/com/darfik/taskmanager/task/service/TaskService.java
+++ src/main/java/com/darfik/taskmanager/task/service/TaskService.java
@@ -1,4 +1,11 @@
 package com.darfik.taskmanager.task.service;
 
+import com.darfik.taskmanager.task.entity.Task;
+
+import java.util.List;
+
 public interface TaskService {
+
+    public List<Task> findAllTasks();
+
 }
