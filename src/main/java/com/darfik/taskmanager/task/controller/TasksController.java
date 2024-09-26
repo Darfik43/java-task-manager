e3ffa88e095231592cc6f607d0c530203d89462c
Darfik43 <darfik43@gmail.com>
2024-09-26 23:07:16 +0300

    feat(controller): add controller method

--- src/main/java/com/darfik/taskmanager/task/controller/TasksController.java
+++ src/main/java/com/darfik/taskmanager/task/controller/TasksController.java
@@ -1,13 +1,24 @@
 package com.darfik.taskmanager.task.controller;
 
+import com.darfik.taskmanager.task.entity.Task;
 import com.darfik.taskmanager.task.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
 public class TasksController {
 
     private final TaskService taskService;
+
+    @GetMapping
+    public List<Task> findTasks() {
+        return taskService.findAllTasks();
+    }
 
 }
