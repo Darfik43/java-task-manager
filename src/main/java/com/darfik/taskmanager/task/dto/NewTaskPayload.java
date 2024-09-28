fc3d341f8183ecdd8f132ebb55e95268794650bd
Darfik43 <darfik43@gmail.com>
2024-09-28 21:42:43 +0300

    feat(dto): add error description

--- src/main/java/com/darfik/taskmanager/task/dto/NewTaskPayload.java
+++ src/main/java/com/darfik/taskmanager/task/dto/NewTaskPayload.java
@@ -6,10 +6,10 @@ import jakarta.validation.constraints.Size;
 public record NewTaskPayload(
 
         @NotNull
-        @Size(min = 3, max = 50)
+        @Size(min = 3, max = 50, message = "Title can not be less than 3 symbols and more than 50 symbols")
         String title,
 
-        @Size(max = 1000)
+        @Size(max = 1000, message = "Title can not be more than 1000 symbols")
         String details
 
 ) {
