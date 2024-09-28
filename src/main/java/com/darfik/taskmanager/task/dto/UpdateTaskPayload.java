2a9a139cd2d578531d6f556e57bc52fa5e3ba589
Darfik43 <darfik43@gmail.com>
2024-09-28 22:16:08 +0300

    feat(dto): create dto to update task

---
+++ src/main/java/com/darfik/taskmanager/task/dto/UpdateTaskPayload.java
@@ -0,0 +1,15 @@
+package com.darfik.taskmanager.task.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdateTaskPayload(
+
+        @NotNull(message = "Title can not be null")
+        @Size(min = 3, max = 50, message = "Details can not be more than 50 symbols and less than 3 symbols")
+        String title,
+        @Size(max = 1000, message = "Details can not be more than 1000 symbols")
+        String details
+
+) {
+}
