58e3b0b1220a8c453ba33e3252a46f398b51c8dd
Darfik43 <darfik43@gmail.com>
2024-09-28 21:41:41 +0300

    feat(controller): implement error handling

--- src/main/java/com/darfik/taskmanager/task/controller/TasksController.java
+++ src/main/java/com/darfik/taskmanager/task/controller/TasksController.java
@@ -5,12 +5,16 @@ import com.darfik.taskmanager.task.entity.Task;
 import com.darfik.taskmanager.task.service.TaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 @RestController
@@ -26,11 +30,21 @@ public class TasksController {
     }
 
     @PostMapping
-    public ResponseEntity<Task> createTask(@Valid @RequestBody NewTaskPayload newTaskPayload,
+    public ResponseEntity<?> createTask(@Valid @RequestBody NewTaskPayload newTaskPayload,
                                            BindingResult bindingResult,
-                                           UriComponentsBuilder uriComponentsBuilder) {
+                                           UriComponentsBuilder uriComponentsBuilder,
+                                           Locale locale) {
         if (bindingResult.hasErrors()) {
+            ProblemDetail problemDetail = ProblemDetail
+                    .forStatusAndDetail(HttpStatus.BAD_REQUEST, "Bad title or details");
+            problemDetail.setProperty("errors",
+                    bindingResult.getAllErrors()
+                            .stream()
+                            .map(ObjectError::getDefaultMessage)
+                            .toList());
 
+            return ResponseEntity.badRequest()
+                    .body(problemDetail);
         } else {
             Task task = this.taskService.createTask(newTaskPayload.title(), newTaskPayload.details());
             return ResponseEntity
