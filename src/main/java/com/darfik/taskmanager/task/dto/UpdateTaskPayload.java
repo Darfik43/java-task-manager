4e407d1dcc7bf8941d32b57665cbc0ab669c0a4a
Darfik43 <darfik43@gmail.com>
2024-09-29 21:54:58 +0300

    style(dto): delete whitespace

--- src/main/java/com/darfik/taskmanager/task/dto/UpdateTaskPayload.java
+++ src/main/java/com/darfik/taskmanager/task/dto/UpdateTaskPayload.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateTaskPayload(
-
         @NotNull(message = "Title can not be null")
         @Size(min = 3, max = 50, message = "Details can not be more than 50 symbols and less than 3 symbols")
         String title,
