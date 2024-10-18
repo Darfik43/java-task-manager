0175eae842fe040c810124b482edac2e925f14d9
Darfik43 <darfik43@gmail.com>
2024-10-18 23:13:10 +0300

    style(dto):

--- src/main/java/com/darfik/taskmanager/task/dto/NewTaskPayload.java
+++ src/main/java/com/darfik/taskmanager/task/dto/NewTaskPayload.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record NewTaskPayload(
-
         @NotNull
         @Size(min = 3, max = 50, message = "Title can not be less than 3 symbols and more than 50 symbols")
         String title,
