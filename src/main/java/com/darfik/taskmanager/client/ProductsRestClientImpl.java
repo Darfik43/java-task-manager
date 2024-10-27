d07ff015dada20d9113c3ad67e7e5cdc509ec182
Darfik43 <darfik43@gmail.com>
2024-10-27 20:51:12 +0300

    feat(client): implement find method for restClient

---
+++ src/main/java/com/darfik/taskmanager/client/ProductsRestClientImpl.java
@@ -0,0 +1,48 @@
+package com.darfik.taskmanager.client;
+
+import com.darfik.taskmanager.task.entity.Task;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class ProductsRestClientImpl implements ProductsRestClient {
+
+    private static final ParameterizedTypeReference<List<Task>> TASKS_TYPE_REFERENCE
+            = new ParameterizedTypeReference<List<Task>>() {
+    };
+
+    private final RestClient restClient;
+
+    @Override
+    public List<Task> findAllTasks() {
+        return this.restClient
+                .get()
+                .uri("/api/v1/tasks")
+                .retrieve()
+                .body(TASKS_TYPE_REFERENCE);
+    }
+
+    @Override
+    public Task createTask(String title, String details) {
+        return null;
+    }
+
+    @Override
+    public Optional<Task> findTask(Long taskId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void updateTask(Long taskId, String title, String details) {
+
+    }
+
+    @Override
+    public void deleteProduct(Long taskId) {
+
+    }
+}
