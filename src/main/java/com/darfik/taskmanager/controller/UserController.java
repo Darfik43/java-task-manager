d5af5430ba2f73f5b97f95017c55c8e6dee4ce49
Darfik43 <darfik43@gmail.com>
2024-12-04 23:18:05 +0300

    feature: create UserController and implement one method to return user info

---
+++ src/main/java/com/darfik/taskmanager/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.darfik.taskmanager.controller;
+
+import com.darfik.taskmanager.entity.User;
+import com.darfik.taskmanager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    // Now returns entity itself just to test, must be changed
+    @GetMapping
+    public User getUser(Authentication authentication) {
+        return userService.getByUsername(authentication.getName());
+    }
+
+}
