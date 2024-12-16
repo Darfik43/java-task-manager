0845332496bbc8dac0cf768576be84f27f55974e
Darfik43 <darfik43@gmail.com>
2024-12-16 21:40:32 +0300

    fix: lazy init on Role

--- src/main/java/com/darfik/taskmanager/security/JwtUserDetailsService.java
+++ src/main/java/com/darfik/taskmanager/security/JwtUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +17,10 @@ public class JwtUserDetailsService implements UserDetailsService {
 
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.getByUsername(username);
+        user.getRoles().size(); // force initializing Roles
         return JwtEntityFactory.create(user);
     }
 }
