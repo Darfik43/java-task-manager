d9c562b13c7206ee85d3b38c6f7d74b86b9db908
Darfik43 <darfik43@gmail.com>
2024-12-18 22:11:12 +0300

    feat: update with isFinished boolean to update task

--- src/main/java/com/darfik/taskmanager/client/ProductsRestClientImpl.java
+++ src/main/java/com/darfik/taskmanager/client/ProductsRestClientImpl.java
@@ -1,20 +1,24 @@
 package com.darfik.taskmanager.client;
 
 import com.darfik.taskmanager.dto.task.NewTaskPayload;
+import com.darfik.taskmanager.dto.task.UpdateTaskPayload;
 import com.darfik.taskmanager.entity.Task;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 public class ProductsRestClientImpl implements ProductsRestClient {
 
     private static final ParameterizedTypeReference<List<Task>> TASKS_TYPE_REFERENCE
-            = new ParameterizedTypeReference<List<Task>>() {
+            = new ParameterizedTypeReference<>() {
     };
 
     private final RestClient restClient;
@@ -30,27 +34,59 @@ public class ProductsRestClientImpl implements ProductsRestClient {
 
     @Override
     public Task createTask(String title, String details) {
-        return this.restClient
-                .post()
-                .uri("/api/v1/tasks")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(new NewTaskPayload(title, details))
-                .retrieve()
-                .body(Task.class);
+        try {
+            return this.restClient
+                    .post()
+                    .uri("/api/v1/tasks")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new NewTaskPayload(title, details))
+                    .retrieve()
+                    .body(Task.class);
+        } catch (HttpClientErrorException.BadRequest exception) {
+            ProblemDetail problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
+            throw new BadRequestException((List<String>) problemDetail.getProperties().get("errors"));
+        }
     }
 
     @Override
     public Optional<Task> findTask(Long taskId) {
-        return Optional.empty();
+        try {
+            return Optional.ofNullable(this.restClient.get()
+                    .uri("/api/v1/tasks/{taskId}", taskId)
+                    .retrieve()
+                    .body(Task.class));
+        } catch (HttpClientErrorException.NotFound exception) {
+            return Optional.empty();
+        }
+
     }
 
     @Override
-    public void updateTask(Long taskId, String title, String details) {
-
+    public void updateTask(Long taskId, String title, String details, boolean isFinished) {
+        try {
+            this.restClient
+                    .patch()
+                    .uri("/api/v1/tasks/{taskId", taskId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new UpdateTaskPayload(title, details, isFinished))
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (HttpClientErrorException.BadRequest exception) {
+            ProblemDetail problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
+            throw new BadRequestException((List<String>) problemDetail.getProperties().get("errors"));
+        }
     }
 
     @Override
     public void deleteProduct(Long taskId) {
-
+        try{
+            this.restClient
+                    .delete()
+                    .uri("/api/v1/tasks")
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (HttpClientErrorException.NotFound exception) {
+            throw new NoSuchElementException(exception);
+        }
     }
 }
